84bac9f1f81326feaaf244a8c17f3a3ca4821222
linny <997925564@qq.com>
2024-10-28 23:02:12 +0800

    backend: user_list_page

--- backend/backend.py
+++ backend/backend.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 import requests
 from pymongo import MongoClient
 
@@ -14,7 +14,32 @@ clients = dict(
     ]
     )
 app = Flask(__name__)
-ITEM_PER_PAGE = 100
+ITEM_PER_PAGE = 40
+
+db1_user_count, db2_user_count = 0, 0
+for client in clients["db1"]:
+    try:
+        db1_user_count = client.info.user.count_documents({})
+    except:
+        pass
+for client in clients["db2"]:
+    try:
+        db2_user_count = client.info.user.count_documents({})
+    except:
+        pass
+user_list_page_num = (db1_user_count + db2_user_count) // ITEM_PER_PAGE
+if (db1_user_count + db2_user_count) % ITEM_PER_PAGE != 0:
+    user_list_page_num += 1
+
+db2_article_count = 0
+for client in clients["db2"]:
+    try:
+        db2_article_count = client.info.article.count_documents({})
+    except:
+        pass
+article_list_page_num = db2_article_count // ITEM_PER_PAGE
+if db2_article_count % ITEM_PER_PAGE != 0:
+    article_list_page_num += 1
 
 
 # text, image and video files of an article is stored in the dfs
@@ -32,8 +57,7 @@ def user_by_id(uid):
     for client in sum(list(clients.values()),[]):
         try:
             user = client.info.user.find_one({"uid": uid})
-            if user:
-                return user
+            return user
         except:
             pass
     return {"message": f"User {uid} not found."}
@@ -47,8 +71,13 @@ def article_by_id(aid):
             text_path = find_file_path(text_file).replace("0.0.0.0", "nginx").strip()
             text = requests.get(text_path).text
             images = [find_file_path(i).replace("0.0.0.0", "localhost").strip() for i in article["image"].split(",") if i.strip()]
-            videos = [find_file_path(i).replace("0.0.0.0", "localhost").strip() for i in article["video"].split(",") if i.strip()] # in the form of url in flv
-            return dict(text=text, images=images, videos=videos)
+            videos = [find_file_path(i).replace("0.0.0.0", "localhost").strip() for i in article["video"].split(",") if i.strip()]
+            ret = {
+                "text": text, 
+                "images": images, 
+                "videos": videos,
+            }
+            return ret
         except:
             pass
         
@@ -64,39 +93,72 @@ def find_user_read_list(uid):
     return []
 
 
+@app.route("/frontend/user_list/", methods=["GET"])
+def get_user_list_page():
+    pageid = request.args.get("pageid")
+    return user_list_page(pageid)
+    
+
 @app.route("/frontend/user_list/<pageid>")
 def user_list_page(pageid: int):
+    pageid = int(pageid)  # [(pageid-1)*ITEM_PER_PAGE+1, pageid*ITEM_PER_PAGE]
     if not pageid:
         pageid = 1
-    for client in clients["db1"]:
-        try:
-            db1_user_count = len(list(client.info.user.find()))
-        except:
-            pass
-    if pageid * ITEM_PER_PAGE < db1_user_count:
-        target_clients = clients["db1"]
-    else:
-        target_clients = clients["db2"]
-    for client in target_clients:
-        try:
-            res = client.info.user.find().skip((pageid - 1) * ITEM_PER_PAGE - 1).limit(ITEM_PER_PAGE)
-            user_list = list(res)
-        except:
-            pass
-    return render_template("user_list.html", user_list=user_list)
+    
+    le, ri = (pageid - 1) * ITEM_PER_PAGE + 1, pageid * ITEM_PER_PAGE
+    if ri <= db1_user_count:
+        skipnum = le - 1
+        for client in clients["db1"]:
+            # try:
+                res = client.info.user.find().skip(skipnum).limit(ITEM_PER_PAGE)
+                user_list = list(res)
+            # except:
+            #     pass
+    elif le > db1_user_count:
+        skipnum = le - 1 - db1_user_count
+        for client in clients["db2"]:
+            # try:
+                res = client.info.user.find().skip(skipnum).limit(ITEM_PER_PAGE)
+                user_list = list(res)
+            # except:
+            #     pass
+    else:  # le <= db1_user_count < ri
+        skipnum = le - 1
+        user_list = []
+        for client in clients["db1"]:
+            # try:
+                res = client.info.user.find().skip(skipnum)
+                user_list = list(res)
+            # except:
+            #     pass
+        for client in clients["db2"]:
+            # try:
+                res = client.info.user.find().limit(ri - db1_user_count)
+                user_list.extend(list(res))
+            # except:
+            #     pass
+    return render_template("user_list.html", pageid=pageid, user_list=user_list, item_per_page=ITEM_PER_PAGE, total_page_num=user_list_page_num)
 
+
+@app.route("/frontend/article_list/", methods=["GET"])
+def get_article_list_page():
+    pageid = request.args.get("pageid")
+    return article_list_page(pageid)
+    
 
 @app.route("/frontend/article_list/<pageid>")
 def article_list_page(pageid: int):
+    pageid = int(pageid)
     if not pageid:
         pageid = 1
+        
     for client in clients["db2"]:
-        try:  # in case one of the db brokedown
-            res = client.info.article.find().skip((pageid - 1) * ITEM_PER_PAGE - 1).limit(ITEM_PER_PAGE)
+        # try:  # in case one of the db brokedown
+            res = client.info.article.find().skip((pageid - 1) * ITEM_PER_PAGE).limit(ITEM_PER_PAGE)
             article_list = list(res)
-        except:
-            pass
-    return render_template("article_list.html", article_list=article_list)
+        # except:
+        #     pass
+    return render_template("article_list.html", pageid=pageid, article_list=article_list, item_per_page=ITEM_PER_PAGE, total_page_num=article_list_page_num)
 
 
 @app.route("/frontend/search/<search_text>")
